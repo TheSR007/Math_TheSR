81997d6aee603037fcdb1b0715af66abfada0db0
TheSR <101653309+TheSR007@users.noreply.github.com>
2022-12-30 00:44:47 +0600

    Update Nth_Sum_of_any_Number.c

--- Math_TheSR/Nth_Sum_of_any_Number/Nth_Sum_of_any_Number.c
+++ Math_TheSR/Nth_Sum_of_any_Number/Nth_Sum_of_any_Number.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <conio.h>
 int main()
 {
     long long N;
     printf("Enter N to get the sum from 0 to N: ");
     scanf("%lld",&N);
     printf("%lld\n",N*(N+1)/2);//formula for Nth sum N*(N+1)/2
-    
+    getch();
 }
